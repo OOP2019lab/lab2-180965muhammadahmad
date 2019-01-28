b3480450abe309cc9b663f27a47084cf3d63817b
180965muhammadahmad <46881546+180965muhammadahmad@users.noreply.github.com>
2019-01-28 10:55:33 +0500

    Add files via upload

---
+++ gitHubUser.h
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+struct gitHubUser
+{
+	string firstName;
+	string username;
+	string email;
+	int countfolder;
+	string *githubfolders;
+
+};
---
+++ lab 2 question 2.cpp
@@ -0,0 +1,37 @@
+// lab 2 question 2.cpp : Defines the entry point for the console application.
+//
+#include <fstream>
+#include <iostream>
+#include <string>
+#include "gitHubUser.h"
+using namespace std;
+// thuis function takes input from file 
+void readDataFromFile( gitHubUser * users, string filepath) 
+{
+	ifstream in;
+	in.open(filepath);
+	int size;
+	in>>size;
+	users=new gitHubUser[size];
+	for(int i=0;i<size;i++)
+	{
+		in>>users[i].firstName;
+		in>>users[i].username;
+		in>>users[i].email;
+		in>>users[i].countfolder;
+	
+	
+	}
+
+
+
+}
+int main()
+{
+
+	gitHubUser *classroom;
+	readDataFromFile(classroom, "input.txt");
+	
+	return 0;
+}
+
